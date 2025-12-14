751a347d0d7e4fbdf98038a71873d17047061096
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-12-14 21:03:48 +0200

    creare active payment item

--- lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
   const PaymentDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        SizedBox(height: 12),
+        Container(
+          width: 103,
+          height: 62,
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              side: BorderSide(width: 1.50, color: const Color(0xFF34A853)),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            shadows: [
+              BoxShadow(
+                color: Color(0xFF34A853),
+                blurRadius: 4,
+                offset: Offset(0, 0),
+                spreadRadius: 0,
+              ),
+            ],
+          ),
+          // child: Container(
+          //   decoration: BoxDecoration(
+          //     borderRadius: BorderRadius.circular(15),
+          //     color: Colors.white,
+          //   ),
+          child: Center(child: SvgPicture.asset('assets/images/card.svg')),
+        ),
+        // ),
+      ],
+    );
   }
 }
