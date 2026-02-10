ea9d3aa066878999951475ce94b1c1add983f04d
abdulrahmanibrahimdawood <kabdo2223@gmail.com>
2026-02-10 21:09:06 +0200

    impl create payment intent

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio = Dio();
+
+  Future<Response> post({
+    required body,
+    required String url,
+    required String token,
+    String? contentType,
+  }) async {
+    var response = await dio.post(
+      url,
+      data: body,
+      options: Options(
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': contentType,
+        },
+      ),
+    );
+    return response;
+  }
+}
